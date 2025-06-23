9925f27d005c747376c3bfccf8c1dd4402e4d596
Tomasz <tom.frontz@gmail.com>
2025-06-23 11:08:01 +0200

    Superuser and gitignore added

--- account/views.py
+++ account/views.py
@@ -25,7 +25,7 @@ def user_login(request):
                 return HttpResponse('Niepoprawne dane uwierzytelniające')
         else:
             form = LoginForm()
-    return render(request, 'templates/login.html', {'form': form})
+    return render(request, 'account/login.html', {'form': form})
 
 
 
